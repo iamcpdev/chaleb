19d383c2c504c216829ec3f52c9985e784bd612c
Chaleb Pommells <chaleb@iamcpdev.me>
2021-02-19 13:39:04 -0500

    Added new text to access full resume

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var resumePrompts = {
 };
 
 function main() {
-    console.log("Hello,My name is Chaleb(they/them) and welcome to my resume");
+    console.log("Hello,My name is Chaleb(they/them) and welcome to my resume. To view the PDF copy shoot me an email");
     resumeHandler();
 }
 
